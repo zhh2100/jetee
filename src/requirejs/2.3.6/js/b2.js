88a648378d6c4880d0b810c0498f02cecfc437fb
zhh2100 <112738102@qq.com>
2019-12-31 18:43:48 +0800

    开始php分支

--- src/requirejs/2.3.6/js/b2.js
+++
@@ -1,9 +0,0 @@
-define([],function(){
-	var p = document.createElement('p');
-	p.innerHTML = 'b2.js';
-	document.body.appendChild(p);
-
-	return function(){
-		alert('我是b2.js导出的方法');
-	};
-});
--- src/requirejs/2.3.6/js/b3.js
+++
@@ -1,9 +0,0 @@
-define([],function(){
-	var p = document.createElement('p');
-	p.innerHTML = 'b3.js';
-	document.body.appendChild(p);
-
-	return function(){
-		alert('我是b3.js导出的方法');
-	};
-});
