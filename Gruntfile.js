ca6d80e005bc7fa73694d239fc5f882382a64418
zhh2100 <112738102@qq.com>
2020-01-07 21:58:54 +0800

    先保存

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,55 +10,132 @@ module.exports = function(grunt){
 		clean: {
 			dist: 'dist/<%= pkg.version %>'
 		},
-		concat: {
-            requirejs:{
-                options: {
+		/*concat: {
+			requirejs:{
+				options: {
 					banner: banner.replace('Jetee','requirejs')
-                },
-                src:  ["src/requirejs/require.js"],  //合并哪些js文件
-                dest: "dist/<%= pkg.version %>/requirejs/requirejs.js" //输出的js文件
-            },
-            jquery:{
-                options: {
-                    banner: banner.replace('Jetee','jquery')
-                },
-                src:  ["src/jquery/*.js"],  //合并哪些js文件
-                dest: "dist/<%= pkg.version %>/jquery/jquery.js" //输出的js文件
-            },
-            jetee:{
-                options: {
+				},
+				src:  ["src/requirejs/require.js"],  //合并哪些js文件
+				dest: "dist/<%= pkg.version %>/requirejs/requirejs.js" //输出的js文件
+			},
+			jquery:{
+				options: {
+					banner: banner.replace('Jetee','jquery')
+				},
+				src:  ["src/jquery/*.js"],  //合并哪些js文件
+				dest: "dist/<%= pkg.version %>/jquery/jquery.js" //输出的js文件
+			},
+			jetee:{
+				options: {
 					 banner: banner.replace('Jetee',Jetee)
-                },
-                src:  ["dist/<%= pkg.version %>/requirejs/requirejs.js","dist/<%= pkg.version %>/jquery/jquery.js",],  //合并哪些js文件
-                dest: "dist/<%= pkg.version %>/jetee.js" //输出的js文件
-            }
-        },
-        uglify: {
-            requirejs: {
-                options: {
-                    banner: banner.replace('Jetee','requirejs')
-                },
-                files: {
-                    'dist/<%= pkg.version %>/requirejs/requirejs.min.js': ['src/requirejs/requirejs.js']
-                }
-            },
-            jquery: {
-                options: {
-                    banner: banner.replace('Jetee','jquery')
-                },
-                files: {
-                    'dist/<%= pkg.version %>/jquery/jquery.min.js': ['src/jquery/jquery.js']
-                }
-            },
-            jetee: {
-                options: {
-                    banner: banner.replace('Jetee',Jetee)
-                },
-                files: {
-                    'dist/<%= pkg.version %>/jetee.min.js': ['src/requirejs/requirejs.js','src/jquery/jquery.js']
-                }
-            }
-        },
+				},
+				src:  ["dist/<%= pkg.version %>/requirejs/requirejs.js","dist/<%= pkg.version %>/jquery/jquery.js",],  //合并哪些js文件
+				dest: "dist/<%= pkg.version %>/jetee.js" //输出的js文件
+			}
+		},*/
+		jshint: {
+		  options: {
+			jshintrc: 'src/bootstrap/js/.jshintrc'
+		  },
+		  grunt: {
+			options: {
+			  jshintrc: 'grunt/.jshintrc'
+			},
+			src: ['Gruntfile.js']
+		  },
+		  core: {
+			src: 'src/bootstrap/js/*.js'
+		  }		  
+		},
+		jscs: {
+		  options: {
+			config: 'src/bootstrap/js/.jscsrc'
+		  }		
+		},
+		/*concat: {
+		  options: {
+			banner: banner.replace('Jetee','bootstrap'),
+			stripBanners: true
+		  },
+		  bootstrap: {
+			src: [
+				'js/transition.js',
+				'js/alert.js',
+				'js/button.js',
+				'js/carousel.js',
+				'js/collapse.js',
+				'js/dropdown.js',
+				'js/modal.js',
+				'js/tooltip.js',
+				'js/popover.js',
+				'js/scrollspy.js',
+				'js/tab.js',
+				'js/affix.js'
+			],
+			dest: 'dist/<%= pkg.version %>/bootstrap/js/bootstrap.js'
+		  }
+		},*/
+		uglify: {
+			concat: {
+				options: {
+					mangle:false,
+					compress: false,
+					beautify:true,
+					banner: banner.replace('Jetee',Jetee)
+				},
+				files: {
+					'dist/<%= pkg.version %>/requirejs/requirejs.js': ['src/requirejs/requirejs.js'],
+					'dist/<%= pkg.version %>/jquery/jquery.js': ['src/jquery/jquery.js'],
+					'dist/<%= pkg.version %>/jquery/bootstrap.js': [
+																	'src/bootstrap/js/transition.js',
+																	'src/bootstrap/js/alert.js',
+																	'src/bootstrap/js/button.js',
+																	'src/bootstrap/js/carousel.js',
+																	'src/bootstrap/js/collapse.js',
+																	'src/bootstrap/js/dropdown.js',
+																	'src/bootstrap/js/modal.js',
+																	'src/bootstrap/js/tooltip.js',
+																	'src/bootstrap/js/popover.js',
+																	'src/bootstrap/js/scrollspy.js',
+																	'src/bootstrap/js/tab.js',
+																	'src/bootstrap/js/affix.js'
+																],
+					'dist/<%= pkg.version %>/jetee.js': ['src/requirejs/requirejs.js','src/jquery/jquery.js']
+				}
+			}        	
+			requirejs: {
+				options: {
+					banner: banner.replace('Jetee','requirejs')
+				},
+				files: {
+					'dist/<%= pkg.version %>/requirejs/requirejs.min.js': ['src/requirejs/requirejs.js']
+				}
+			},
+			jquery: {
+				options: {
+					banner: banner.replace('Jetee','jquery')
+				},
+				files: {
+					'dist/<%= pkg.version %>/jquery/jquery.min.js': ['src/jquery/jquery.js']
+				}
+			},
+			bootstrap: {
+				options: {
+					banner: banner.replace('Jetee','bootstrap')
+				},
+				files: {
+					'dist/<%= pkg.version %>/bootstrap/bootstrap.min.js': ['dist/<%= pkg.version %>/jquery/bootstrap.js']
+				}
+			},		
+			jetee: {
+				options: {
+					banner: banner.replace('Jetee',Jetee)
+				},
+				files: {
+					'dist/<%= pkg.version %>/jetee.min.js': ['src/requirejs/requirejs.js','src/jquery/jquery.js']
+				}
+			}
+		},
 		less: {
 			options: {
 				ieCompat: true,
@@ -120,7 +197,7 @@ module.exports = function(grunt){
 	// 2. 加载插件任务
 	require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
 	// 3. 注册构建任务
-	grunt.registerTask('default', ['clean','uglify','less','postcss','cssmin']);
+	grunt.registerTask('default', ['clean','jshint','jscs','uglify','less','postcss','cssmin']);
 	grunt.registerTask('bootstrap', ['clean','less','postcss','cssmin']);
 	grunt.registerTask('stylelint', ['stylelint']);
 };
